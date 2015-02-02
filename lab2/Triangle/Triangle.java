ad3a0394c625ef384331fe5ae21af486fae67dc6
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2015-02-01 21:19:21 -0800

    Finished Triangle

--- lab2/Triangle/Triangle.java
+++ lab2/Triangle/Triangle.java
@@ -3,42 +3,41 @@
  */
 public class Triangle {
 
-  int s1;
-  int s2;
-  int s3;
+    int s1;
+    int s2;
+    int s3;
 
-  public Triangle(int side1, int side2, int side3) {
-    s1 = side1;
-    s2 = side2;
-    s3 = side3;
-  }
-
-  /** Returns a String describing the type of triangle. */
-  public String triangleType() {
-
-    // Chceck that nothing is negative
-    if (s1 <= 0 || s2 <= 0 || s3 <= 0) {
-      return "At least one length is less than 0!";
+    public Triangle(int side1, int side2, int side3) {
+        s1 = side1;
+        s2 = side2;
+        s3 = side3;
     }
 
-    // Check for side length
-    if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
-      return "The lengths of the triangles do not form a valid triangle!";
-    }  
+    /** Returns a String describing the type of triangle. */
+    public String triangleType() {
 
-    // Return triangle type
-    //TODO:  return either "Equilateral", "Isosceles", or "Scalene"
-    if (s1 == s2 | s2 == s3){
-        if (s1 == s2 & s2 == s3){
-            return "Equilateral";
+        // Chceck that nothing is negative
+        if (s1 <= 0 || s2 <= 0 || s3 <= 0) {
+            return "At least one length is less than 0!";
+        }
+
+        // Check for side length
+        if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
+            return "The lengths of the triangles do not form a valid triangle!";
+        }  
+
+        // Return triangle type
+        //TODO:  return either "Equilateral", "Isosceles", or "Scalene"
+        if (s1 == s2 | s2 == s3 | s1 == s3){
+            if (s1 == s2 & s2 == s3){
+                return "Equilateral";
+            }
+            else {
+                return "Isoceles";
+            }
         }
         else {
-            return "Isosceles";
-        }
+            return "Scalene";
+        } 
     }
-    else {
-
-    return "Scalene";
-    } 
-  }
 }
