2f6330f86ef69ccf35a6190a7b668e7b9ed2cc6c
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2015-03-13 23:13:01 -0700

    finished except zipf?

--- proj1/ngordnet/WordLengthProcessor.java
+++ proj1/ngordnet/WordLengthProcessor.java
@@ -3,12 +3,14 @@ class WordLengthProcessor implements YearlyRecordProcessor {
     @Override
     public double process(YearlyRecord yr){
         long wordLengthSum = 0;
+        long numWords = 0;
         for (String s : yr.words()){
             wordLengthSum += s.length() * yr.count(s);
+            numWords += yr.count(s);
         }
         if (yr.size() == 0) {
             return 0;
         }
-        return wordLengthSum / ((double) yr.size());
+        return wordLengthSum / ((double) numWords);
     }
 }
