152436cec5a59d49d5fc7f0aa428b2e0980579ef
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2015-04-19 20:34:24 -0700

    finished radix

--- lab12/radix/Sorts.java
+++ lab12/radix/Sorts.java
@@ -22,6 +22,26 @@ public class Sorts {
      *    and containing the same keys sorted according to the chosen digit.
      **/
     public static int[] countingSort(int[] keys, int whichDigit) {
+        int mask = 15 << (4 * whichDigit);
+        //initialized to 0?
+        int[] countArray = new int[16];
+        int[] result = new int[keys.length];
+        int bits;
+        int index;
+        for (int x : keys) {
+            bits = (x & mask) >> (4 * whichDigit);
+            countArray[bits]++;
+        }
+        for (int i = 1; i < 16; i++){
+            countArray[i] = countArray[i] + countArray[i-1];
+        }
+        for (int x : keys) {
+            bits = (x & mask) >> (4 * whichDigit);
+            index = countArray[bits] - 1;
+            countArray[bits]++;
+            result[index] = x;
+        }
+        return result;
         //YOUR CODE HERE
     }
 
@@ -34,6 +54,10 @@ public class Sorts {
      *    and containing the same keys in sorted order.
      **/
     public static int[] radixSort(int[] keys) {
+        for(int i=0;i<8;i++) {
+            keys = countingSort(keys, i);
+        }
+        return keys;
         //YOUR CODE HERE
     }
 
