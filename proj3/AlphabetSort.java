f3c598efd370f8e36355d6bcaaaf3bef1744fb43
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2015-04-29 12:44:04 -0700

    asdf

--- proj3/AlphabetSort.java
+++ proj3/AlphabetSort.java
@@ -3,7 +3,17 @@ public class AlphabetSort {
     public static void main(String[] args){
         //use stringbuider and push & pop as you traverse the trie
         Scanner in = new Scanner(System.in);
+        if (!in.hasNextLine()) {
+            throw new IllegalArgumentException();
+        }
         String alphabet = in.nextLine();
+        Set<Character> s = new Set();
+        for (int i=0; i<alphabet.length();i++){
+            s.add(alphabet.charAt(i));
+        }
+        if (!in.hasNextLine() || alphabet.length() != s.size()) {
+            throw new IllegalArgumentException();
+        }
         Trie t = new Trie();
         while(in.hasNextLine()) {
             t.insert(in.nextLine());
